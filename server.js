7c028c2b4b99f7233be312950330e46833b9756f
Nicola Gaioni <nicog.designer@gmail.com>
2021-02-01 22:53:45 +0100

    pt1

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const { getImages } = require("./sql/db"); //ho db?
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/images", (req, res) => {
+    // console.log("/images route has been hit!!!");
+    // res.json - how we send a response to the client!
+    getImages()
+        .then(({ rows }) => res.json(rows))
+        .catch((err) => console.log("err with getImages: ", err));
+});
+
+app.get("/*", (req, res) => res.redirect("/"));
+app.listen(8080, () => console.log("IB server is listening..."));
